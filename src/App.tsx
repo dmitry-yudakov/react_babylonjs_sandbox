2c45074bde8f4e2b2a901bef19a5ce1c8c346ede
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2023-04-07 10:18:58 +0300

    Initial version with many instances of objects

---
+++ src/App.tsx
@@ -0,0 +1,283 @@
+import { Quaternion, Vector3 } from '@babylonjs/core/Maths/math.vector';
+import { HDRCubeTexture, Mesh, PBRMaterial } from '@babylonjs/core';
+import { Scene, Engine, useBeforeRender, useScene } from 'react-babylonjs';
+import { FC, memo, useEffect, useMemo, useRef, useState } from 'react';
+import {
+  Color3,
+  FreeCameraKeyboardMoveInput,
+  MeshBuilder,
+  UniversalCamera,
+} from '@babylonjs/core';
+import hdrImageTexture from './test.hdr';
+
+const RotatingBox: FC<{ pos: Vector3; rpm: number; base: any }> = memo(
+  ({ pos, rpm, base }) => {
+    const scene = useScene();
+    const ref = useRef<Mesh | null>(null);
+
+    const [y, setY] = useState(0);
+    // const [s, setS] = useState(1);
+    // useBeforeRender(() => {
+    // console.log('before render apply position', pos);
+    // if (ref.current) ref.current.position = pos;
+    //   if (!scene) return;
+
+    //   const deltaTimeInMillis = scene.getEngine().getDeltaTime();
+    //   setY(
+    //     (oldY) => oldY + (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000)
+    //   );
+
+    //   // setY((oldY) => oldY + 0.005);
+    //   // setS((oldS) => oldS + 1);
+    // });
+    // console.log('rendering box', { y, s });
+
+    const inst = useMemo(() => {
+      if (!base) return null;
+      const inst = base.createInstance('box' + Math.random());
+      // inst.position = pos;
+      return inst;
+    }, [base]);
+
+    // if (!base) return null;
+
+    // console.log('rendering box', { y, pos, base });
+    return (
+      <mesh
+        // ref={ref}
+        name={'box' + Math.random()}
+        fromInstance={base}
+        // fromInstance={inst}
+        position={pos}
+        disposeInstanceOnUnmount
+      >
+        {/* <transformNode name="sphereTransform" position={pos} /> */}
+        {/* <standardMaterial
+        name="material"
+        diffuseColor={Color3.Yellow()}
+        specularColor={Color3.Black()}
+      /> */}
+      </mesh>
+    );
+    return (
+      <box
+        name="box"
+        size={1}
+        position={pos}
+        // rotation={new Vector3(0, y, 0)}
+        //fromInstance={}
+      >
+        <pbrMaterial
+          name="glass"
+          alpha={0.5}
+          directIntensity={0.0}
+          indexOfRefraction={0.52}
+          cameraExposure={0.66}
+          reflectivityColor={new Color3(0.2, 0.2, 0.2)}
+          albedoColor={new Color3(0.95, 0.95, 0.95)}
+        >
+          <hdrCubeTexture
+            name="skybox"
+            url={hdrImageTexture}
+            size={512}
+            coordinatesMode={0}
+          />
+        </pbrMaterial>
+      </box>
+    );
+  }
+);
+
+const SPREAD = 100;
+
+const generateInitialPos = (size = 1000, max = SPREAD) => {
+  const pos = [];
+  for (let i = 0; i < size; i++) {
+    pos.push(
+      new Vector3(Math.random() * max, Math.random() * max, Math.random() * max)
+    );
+  }
+  return pos;
+};
+
+const Objects = () => {
+  const [objectsPos, setObjectsPos] = useState<Vector3[]>(
+    generateInitialPos(5000, 50)
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setObjectsPos((oldPos) => [
+        ...oldPos,
+        new Vector3(
+          Math.random() * SPREAD,
+          Math.random() * SPREAD,
+          Math.random() * SPREAD
+        ),
+      ]);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  console.log('rendering app', { objectsPos });
+  const scene = useScene();
+
+  const base = useMemo(() => {
+    if (!scene) return null;
+
+    const baseSphere = MeshBuilder.CreateSphere(
+      'Odyssey123',
+      { diameter: 1 },
+      scene
+    );
+    // baseSphere.isVisible = false; // Made the original sphere invisible.
+
+    const hdrTextureAccounts = new HDRCubeTexture(
+      hdrImageTexture,
+      scene,
+      512,
+      false,
+      true,
+      false,
+      true
+    );
+    const glassMaterial = new PBRMaterial('glass', scene);
+    glassMaterial.reflectionTexture = hdrTextureAccounts;
+    glassMaterial.indexOfRefraction = 0.52;
+    glassMaterial.alpha = 0.5;
+    glassMaterial.directIntensity = 0.0;
+    glassMaterial.environmentIntensity = 0.7;
+    glassMaterial.cameraExposure = 0.66;
+    glassMaterial.cameraContrast = 1.66;
+    glassMaterial.microSurface = 1;
+    glassMaterial.reflectivityColor = new Color3(0.2, 0.2, 0.2);
+    glassMaterial.albedoColor = new Color3(0.95, 0.95, 0.95);
+    baseSphere.material = glassMaterial;
+    return baseSphere;
+  }, [scene]);
+
+  return (
+    <transformNode name="gaga">
+      {objectsPos.map((pos, i) => (
+        <RotatingBox key={i} pos={pos} rpm={5} base={base} />
+      ))}
+    </transformNode>
+  );
+};
+
+const Player: FC<{ spawnPoint?: Vector3 }> = ({
+  spawnPoint = new Vector3(0, 1, 0),
+}) => {
+  const cameraRef = useRef<UniversalCamera | null>(null);
+  const sphereTransformRef = useRef<any>(null);
+
+  const [isShiftDown, setIsShiftDown] = useState(false);
+
+  useEffect(() => {
+    if (cameraRef.current && sphereTransformRef.current) {
+      console.log('Attach PLAYER view');
+      const camera = cameraRef.current; //.hostInstance;
+      const sphereTransform = sphereTransformRef.current; //.hostInstance;
+      sphereTransform.setParent(camera);
+      sphereTransform.position = new Vector3(0, -2, 10);
+    }
+
+    // cameraRef.current?.inputs.addMouse();
+    // cameraRef.current?.inputs.addKeyboard();
+    // cameraRef.current?.inputs.addTouch();
+
+    // Speed up camera
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Shift') {
+        console.log('shift down');
+        setIsShiftDown(true);
+      }
+    };
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === 'Shift') {
+        console.log('shift up');
+        setIsShiftDown(false);
+      }
+    };
+
+    window.addEventListener('keydown', onKeyDown);
+    window.addEventListener('keyup', onKeyUp);
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+      window.removeEventListener('keyup', onKeyUp);
+    };
+  }, []);
+
+  return (
+    <>
+      <universalCamera
+        ref={cameraRef}
+        name="UniversalCamera"
+        // position={new Vector3(0, 5, -10)}
+        position={spawnPoint}
+        rotationQuaternion={new Quaternion()}
+        speed={isShiftDown ? 5 : 1}
+        keysUp={[87]}
+        keysDown={[83]}
+        keysLeft={[65]}
+        keysRight={[68]}
+        keysUpward={[69]}
+        keysDownward={[81]}
+      />
+      <sphere
+        ref={sphereTransformRef}
+        name="shinySphere"
+        diameter={0.5}
+        segments={32}
+      >
+        <standardMaterial
+          name="shinySphereMaterial"
+          diffuseColor={Color3.White()}
+          specularColor={Color3.White()}
+        />
+      </sphere>
+    </>
+  );
+};
+
+const App: FC = () => {
+  return (
+    <div style={{ flex: 1, display: 'flex' }}>
+      <Engine
+        antialias
+        adaptToDeviceRatio
+        canvasId="babylon-js"
+        renderOptions={{
+          whenVisibleOnly: true,
+        }}
+      >
+        <Scene>
+          {/* <freeCamera
+            name="camera1"
+            position={new Vector3(0, 5, -10)}
+            setTarget={[Vector3.Zero()]}
+          /> */}
+
+          <hemisphericLight
+            name="light1"
+            intensity={0.7}
+            direction={new Vector3(0, 1, 0)}
+          />
+          <ground name="ground" width={10} height={6} />
+          {/* <box
+            name="box"
+            size={2}
+            position={new Vector3(0, 1, 0)}
+            rotation={Vector3.Zero()}
+          /> */}
+
+          <Player />
+          <Objects />
+
+          <babylon-line name="line" />
+        </Scene>
+      </Engine>
+    </div>
+  );
+};
+export default App;
